8df60b86924d03864d7368fc869eb8a941326d6c
Hannah-Unyolo <unyolohannah@gmail.com>
2024-03-20 09:49:09 +0300

    practiced new functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,6 +15,17 @@ names("Hannah","Konemi","Pempho", "Tina")
     println(name("Hannah"))
 
     subtraction(30, 26)
+
+    //Day 2
+    temperature(56.7)
+
+    information("Hannah",22, "Pink")
+
+    average(3, 6,9)
+    convertTemp(32)
+    println(newAge(22))
+
+    myString(("Hannah"))
     }
 
 
@@ -31,6 +42,35 @@ fun name(word:String):Int{
 fun subtraction(num1: Int,num2:Int ){
     var difference = num1 - num2
     println(difference)
+}
+
+//Day 2
+fun temperature(num:Double){
+    var message = "The temperature is  $num degree Celcius"
+    println(message)
+}
+
+fun information(name:String, age:Int, color: String){
+    var text = (name) +" "+(age )+" "+ (color)
+    println(text)
+}
+fun average(num1:Int, num2:Int, num3: Int){
+    var sum = num1 + num2 + num3
+    var avg = sum / 3
+    println(avg)
+}
+fun convertTemp(num : Int){
+    var converting = num * 1.8 + 32
+    println(converting.toDouble())
+}
+
+fun newAge(age:Int):Int{
+    return age + 1
+}
+fun myString(word:String){
+    println(word.indexOf("a"))
+    println(word.uppercase())
+    println(word.startsWith("z"))
 }
 
 
